4dc6048f0061f9a9df0c28ae4bb613e983f97239
AlexCastino <alexander.feitosa@aluno.faculdadeimpacta.com.br>
2021-11-12 00:25:14 -0300

    'asas'

--- teste.bat
+++ teste.bat
@@ -14,7 +14,7 @@ if %resposta%==%sim% (
 echo Executando comando git add .
 git add .
 
-echo ^<ESC^>[92m [92mExecutando comando git commit -m "'%comentario%'"[0m 
+echo ^<ESC^>[92m [92m Executando comando git commit -m "'%comentario%'" [0m 
 git commit -m "'%comentario%'"
 echo Executando comando git push
 git commit -m "'%comentario%'"
