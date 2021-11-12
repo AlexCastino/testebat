9e479acdcc0dd50e603c1dd39040e70ab21276b1
AlexCastino <alexander.feitosa@aluno.faculdadeimpacta.com.br>
2021-11-12 00:09:57 -0300

    '124'

--- teste.bat
+++ teste.bat
@@ -4,9 +4,9 @@ echo '%comentario%'
 git add .
 git commit -m "'%comentario%'"
 set /p resposta="Deseja subir os arquivos acima em seu repositorio? Sim= s Não= n"
-if %resposta% == 's'(
-git push
-pause)
-if %resposta% != 's'(
-echo Tchau)
+if %resposta%=='s'(
+git push 
+pause
+)
+if %resposta%!='s'(echo Tchau)
 pause
