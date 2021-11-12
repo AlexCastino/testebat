4bac8a790d0eae9f1c24085218a7839b816dd0b1
AlexCastino <alexander.feitosa@aluno.faculdadeimpacta.com.br>
2021-11-12 00:17:58 -0300

    'asa'

--- teste.bat
+++ teste.bat
@@ -1,14 +1,14 @@
 @echo off
 set /p comentario="Digite o comentario: "
-set sim="s"
-set nao='n'
+set sim=s
+set nao=n
 echo '%comentario%'
 echo '%sim%'
 echo '%nao%'
 git add .
 git commit -m "'%comentario%'"
 set /p resposta="Deseja subir os arquivos acima em seu repositorio? Sim= s Não= n : "
-if %resposta%==s(
+if %resposta%==%sim%(
 git push
 echo foi)
 pause
